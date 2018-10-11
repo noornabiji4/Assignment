28f9a17e3fcb4a20e7b99812dd5a0da04f35f5a6
Noor <noornabiji4@gmail.com>
2018-10-11 23:53:54 +0530

    card assignment

---
+++ card/src/card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Card extends React.Component{
+    render(){
+      
+        return(
+            <div className="card">
+            <div className="phone">Phone Features</div>
+            <div className="box">
+            <div className="Subwoofer">Subwoofer</div>
+            <div className="Non">Non-porous, washeble</div>
+            <div className="Wings">Wings</div>
+            <div className="Beveled">Beveled Bazel</div>
+            <div className="Bezeled">Bezeled bevel</div>
+            <div className="Seedless">Seedless</div>
+            </div>
+            </div>
+            
+
+        )
+    }
+}
+
+export default Card

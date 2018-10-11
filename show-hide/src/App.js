a5defb90eac566f1879d612a0819ad0446380870
Noor <noornabiji4@gmail.com>
2018-10-12 00:42:26 +0530

    hide adn show buttons.

---
+++ show-hide/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hide from './hide'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Hide/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
